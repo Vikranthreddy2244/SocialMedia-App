58a763a30e007821fc0004505e72a7bba3d992c3
Vikranthreddy2244 <vikranthreddy2244@gmail.com>
2024-11-07 16:51:15 -0600

    setup mongoose connection

--- index.js
+++ index.js
@@ -40,3 +40,16 @@ const storage = multer.diskStorage({
     },
   });
   const upload = multer({ storage });
+
+
+  /* MONGOOSE SETUP */
+const PORT = process.env.PORT || 8080;
+mongoose
+  .connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () => console.log(`Server is running on port: ${PORT}`));
+  })
+  .catch((error) => console.log(`${error} did not connect`));
