e577e885ddd196c8cb6987fa52ae909540a34fd4
Vikranthreddy2244 <vikranthreddy2244@gmail.com>
2024-12-05 17:54:04 -0600

    added routes

--- index.js
+++ index.js
@@ -9,7 +9,11 @@ import morgan from 'morgan'
 import path from 'path'
 import { fileURLToPath } from 'url'
 import authRoutes from './routes/auth.js'
+import userRoutes from './routes/users.js'
+import postRoutes from './routes/posts.js'
 import { register } from './controllers/auth.js'
+import { createPost } from './controllers/posts.js'
+import { verifyToken } from './middleware/auth.js'
 
 
 /* CONFIGURATIONS */
